eb9635cb42d44b11ab0c3f49775b4f9eb8f67ec7
Service Web <sw@gssmail.com>
2024-03-12 08:59:26 -0500

    Commit changes in C:\Websites\Git\emarlow\4451\GCG_4451_TextM_Maint.sql

---
+++ GCG_4451_TextM_Maint.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "GCG_4451_TextM_Maint"(
+ "Rec_ID" BIGINT,
+ "Group_ID" INTEGER,
+ "Group_Name" CHAR(20),
+ "Sender_Email" CHAR(30),
+ "Message" LONGVARCHAR,
+ "Department" CHAR(11),
+ "Group_Email" CHAR(100),
+ "Critical_Email" CHAR(100),
+ "Send_Crit_Email" BIT);
